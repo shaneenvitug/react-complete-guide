fb77f35a3922936ef035a14fc820ede84d2afe64
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-10 15:56:35 +1100

    passing method references bet components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,46 +1,50 @@
-import React, { useState } from 'react';
-// useState is the hook that allows us to manage state in a functional component
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  // useState returns an array with exactly two elements, 1st element is our current state, 2nd element is a function that lets us update this state
-  const [ personsState, setPersonsState ] = useState({
-    // react hooks, function does not merge whatever you pass to it, it replaces it, manually include old state data
+class App extends Component {
+  // state is managed from inside the component, *state property is only available in components that extends component (class based react components)
+  // props are set and passed from outside
+  state = {
     persons: [
       { name: 'Shaneen', age: 28 },
       { name: 'Mark', age: 29 },
       { name: 'Bianx', age: 16 }
-    ]
-  });
+    ],
+    otherState: 'some other value'
+  }
 
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-  
-
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked');
     // DON'T DO THIS: this.state.persons[0].name = "Samekla";
-    setPersonsState({
+    this.setState({
       persons: [
-        { name: 'Samekla', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Mark', age: 29 },
         { name: 'Bianx', age: 17 }
       ]
     })
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} > My Hobbies: Basketball</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+       <h1>Hi, I'm a React App</h1>
+       <p>This is really working!</p>
+       <button onClick={() => this.switchNameHandler('Samekla')}>Switch Name</button>
+       <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+       <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Nin!')} >My Hobbies: Basketball</Person>
+       <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;

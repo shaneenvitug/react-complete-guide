b3a0631b0252bfac5eac8bfdf8d708f042121817
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-10 13:58:44 +1100

    understanding the state property

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // state is managed from inside the component, *state property is only available in components that extends component (class based react components)
+  // props are set and passed from outside
+  state = {
+    persons: [
+      { name: 'Shaneen', age: 28 },
+      { name: 'Mark', age: 29 },
+      { name: 'Bianx', age: 16 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
        <p>This is really working!</p>
-       <Person name="Shaneen" age="28" />
-       <Person name="Mark" age="29" > My Hobbies: Basketball</Person>
-       <Person name="Bianx" age="16" />
+       <button>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My Hobbies: Basketball</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }

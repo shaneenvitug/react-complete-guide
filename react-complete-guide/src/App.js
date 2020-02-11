52699142eef3b3583602b1ceea9d97408206d23d
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-11 18:11:45 +1100

    handling dynamic content the js way

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
       { name: 'Mark', age: 29 },
       { name: 'Bianx', age: 16 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -36,6 +37,11 @@ class App extends Component {
     })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -45,24 +51,34 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age} 
+            click={this.switchNameHandler.bind(this, 'Nin!')} 
+            changed={this.nameChangedHandler} >My Hobbies: Basketball</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
+        </div> 
+      );
+    }
+
     return (
       <div className="App">
-       <h1>Hi, I'm a React App</h1>
-       <p>This is really working!</p>
-       <button 
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
           style={style}
-          onClick={() => this.switchNameHandler('Samekla')}>Switch Name</button>
-       <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age} />
-       <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age} 
-          click={this.switchNameHandler.bind(this, 'Nin!')} 
-          changed={this.nameChangedHandler} >My Hobbies: Basketball</Person>
-       <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   }

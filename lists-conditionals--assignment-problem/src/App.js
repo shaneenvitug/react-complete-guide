23aff91b16815d6a736c0e031a212f54469ada28
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-12 14:51:16 +1100

    added key to remove console warning

--- lists-conditionals--assignment-problem/src/App.js
+++ lists-conditionals--assignment-problem/src/App.js
@@ -32,19 +32,19 @@ class App extends Component {
     return (
       <div className="App">
         <ol>
-          <li>(DONE)Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-          <li>(DONE)Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
-          <li>(DONE)Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-          <li>(DONE)Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
-          <li>(DONE)Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>(DONE)When you click a CharComponent, it should be removed from the entered text.</li>
+          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+          <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
 
         <input type="text" onChange={this.textLengthHandler} value={this.state.word} />
         <Validation length={this.state.length} />
         {
-          this.state.word.split("").map((letter, index) => <Char letter={letter} onClick={() => this.deleteLetterHandler(index)}/>)
+          this.state.word.split("").map((letter, index) => <Char letter={letter} key={index} onClick={() => this.deleteLetterHandler(index)}/>)
         }
       </div>
     );

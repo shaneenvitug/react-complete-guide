fb28fea98a05083f5e5e9fd6cfe5f732cf065931
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-15 12:36:14 +1100

    component creation lifecycle in action

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -4,6 +4,10 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
   // state is managed from inside the component, *state property is only available in components that extends component (class based react components)
   // props are set and passed from outside
   state = {
@@ -14,6 +18,22 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    
+    return state;
+  }
+
+  componentWillMount() {
+    console.log('[App.js] componentWillMount');
+    
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+    
   }
 
   nameChangedHandler = (event, id) => {
@@ -47,6 +67,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -59,6 +80,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons} 
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />

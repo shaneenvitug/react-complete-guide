043fd1e173412dff7a616f40061674f3638be766
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-15 14:13:20 +1100

    component update lifecycle (for state changes)

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -33,7 +33,15 @@ class App extends Component {
 
   componentDidMount() {
     console.log('[App.js] componentDidMount');
-    
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
   }
 
   nameChangedHandler = (event, id) => {

7ebb342ea2ee17e045d9512b9f4b013fa4367aeb
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-17 16:35:36 +1100

    passing unknown props

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
@@ -16,4 +17,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);

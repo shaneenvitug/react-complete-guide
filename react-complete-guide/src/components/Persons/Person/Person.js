4cacb3234537fa24292120cff0db93b114556a73
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-18 14:19:01 +1100

    using the context api

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
+import AuthContext from '../../../context/auth-context';
 
 class Person extends Component {
   constructor(props) {
@@ -20,7 +21,9 @@ class Person extends Component {
     return (
       
       <Aux>
-        {this.props.isAuth ? <p>Authenticated!</p> : <p>Please log in</p>}
+        <AuthContext.Consumer>
+          {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
+        </AuthContext.Consumer>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input 

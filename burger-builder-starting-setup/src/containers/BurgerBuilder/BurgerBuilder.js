2aa49a6776fd0bd9f43b1fe1648a0664728444c8
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-25 17:27:56 +1100

    navigating to the checkout page

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,6 +30,8 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount () {
+    console.log(this.props);
+    
     axios.get('https://react-my-burger-eac8a.firebaseio.com/ingredients.json')
       .then(response => {
         this.setState({ingredients: response.data})
@@ -91,28 +93,29 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    this.setState({loading: true})
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Shaneen Vitug',
-        address: {
-          street: 'Irwin Crescent',
-          zipCode: '2207',
-          country: 'Australia'
-        },
-        email: 'sjbatugs@gmail.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({loading: false, purchasing: false})
-      })
-      .catch(error => {
-        this.setState({loading: false, purchasing: false})
-      })
+    // this.setState({loading: true})
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Shaneen Vitug',
+    //     address: {
+    //       street: 'Irwin Crescent',
+    //       zipCode: '2207',
+    //       country: 'Australia'
+    //     },
+    //     email: 'sjbatugs@gmail.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({loading: false, purchasing: false})
+    //   })
+    //   .catch(error => {
+    //     this.setState({loading: false, purchasing: false})
+    //   })
+    this.props.history.push('/checkout')
   }
 
   render() {

874798831f815cb001c8808e8466995f9abd3e6a
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-27 21:33:34 +1100

    handling user input

--- burger-builder-starting-setup/src/components/UI/Input/Input.js
+++ burger-builder-starting-setup/src/components/UI/Input/Input.js
@@ -9,19 +9,19 @@ const input = (props) => {
       inputElement = <input 
         className={classes.InputElement} 
         {...props.elementConfig} 
-        value={props.value} />;
+        value={props.value} onChange={props.changed} />;
       break;
     case ('textarea'):
       inputElement = <textarea 
         className={classes.InputElement} 
         {...props.elementConfig} 
-        value={props.value}/>;
+        value={props.value} onChange={props.changed} />;
       break;
     case ('select'):
       inputElement = (
         <select 
           className={classes.InputElement} 
-          value={props.value}>
+          value={props.value} onChange={props.changed} >
           {props.elementConfig.options.map(option => (
             <option key={option.value} value={option.value}>{option.displayValue}</option>
           ))}
@@ -32,7 +32,7 @@ const input = (props) => {
       inputElement = <input 
         className={classes.InputElement} 
         {...props.elementConfig} 
-        value={props.value} />;
+        value={props.value} onChange={props.changed} />;
   }
 
   return (
--- burger-builder-starting-setup/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-starting-setup/src/containers/Checkout/ContactData/ContactData.js
@@ -78,7 +78,18 @@ class ContactData extends Component {
       .catch(error => {
         this.setState({loading: false})
       })
+  }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm})
   }
 
   render () {
@@ -97,7 +108,8 @@ class ContactData extends Component {
             key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value} />
+            value={formElement.config.value} 
+            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>

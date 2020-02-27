4d360eaa5835c985cbdbdc6f5ef65d2e653849b1
Shaneen Vitug <shaneenvitug@gmail.com>
2020-02-27 23:05:23 +1100

    handing overall form validity

--- burger-builder-starting-setup/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-starting-setup/src/containers/Checkout/ContactData/ContactData.js
@@ -83,9 +83,11 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'},
           ]
         },
-        value: ''
+        value: '',
+        valid: true
       }
     },
+    formIsValid: false,
     loading: false
   }
 
@@ -141,8 +143,13 @@ class ContactData extends Component {
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    console.log(updatedFormElement);
-    this.setState({orderForm: updatedOrderForm})
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) { 
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+
+    this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid})
   }
 
   render () {
@@ -168,7 +175,7 @@ class ContactData extends Component {
             touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
 
     );
